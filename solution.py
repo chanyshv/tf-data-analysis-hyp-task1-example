f0e676541eb4c3d5979c0f7d022c7ceff255f52a
chanyshev <chanyshv@gmail.com>
2024-05-25 17:21:18 +0300

    first task

--- solution.py
+++ solution.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+# from scipy.stats import norm
+
+chat_id = 506238275  # Ваш chat ID, не меняйте название переменной
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(control_sales: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    control_suc_rate = control_sales / x_cnt
+    test_suc_rate = y_success / y_cnt
+    z = (test_suc_rate - control_suc_rate) / (
+            (control_suc_rate * (1 - control_suc_rate) * (1 / x_cnt + 1 / y_cnt)) ** 0.5)
+    # z_critical = norm.ppf(1 - 0.08)
+    z_critical = 1.4050715603096329
+
+    return z > z_critical
+
+# print(norm.ppf(1 - 0.08))
